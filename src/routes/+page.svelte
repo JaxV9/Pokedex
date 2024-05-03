9344583a9edf140cd430cf6ffc748d74f48e4297
Aïcha El Hamed <lalla@MacBook-Air-de-Aicha.local>
2024-05-03 13:12:32 +0200

    fonction recherche aicha

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,44 @@
 <script lang="ts">
+	let pokemonName = '';
+	  
+	  // Fonction pour gérer le form normalement
+	  function handleSubmit(event) {
+		event.preventDefault();
+		if (pokemonName.trim() !== '') {
+		  // ouvre la page du Pokémon
+		  window.location.href = `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`;
+		}
+	  }
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Find your pokemon</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
 	<h1>
-		Hello
+		Find your pokemon
 	</h1>
-
 </section>
+	  
+	 
+	  
+	  <!-- bouton pour chercher -->
+		<input
+		  type="text"
+		  placeholder="Rechercher un Pokémon dans le pkedex..."
+		  bind:value={pokemonName}
+		  required
+		/>
+		<a href="/details/{pokemonName}">go to Pokedex</a> 
 
-<style>
-</style>
+
+		<style>
+			/* Ajoute ke style apres*/
+			.search-bar {
+			  display: flex;
+			  align-items: center;
+			  gap: 10px;
+			}
+		  </style>
