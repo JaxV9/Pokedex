dd9e007dbf1a5a8966c308bdf0cd2738d5b91d0f
Zeyoman <alex10pump@gmail.com>
2024-05-03 13:12:21 +0200

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,11 @@
 		const response = await fetch(pokemon.url);
 		const details = await response.json();
 		const sprites = details.sprites.front_default;	
-		console.log(pokemonList)
+		const id = details.id;
+		// console.log(id)
+		// console.log(pokemonList)
 		pokemonDetails = [...pokemonDetails, sprites];
+		// console.log(id)
 	  }
 	}
   
@@ -27,9 +30,12 @@
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </svelte:head>
 
-<section>
+<section id="titre">
 	<h1>
 		Liste Pokemon
 	</h1>
@@ -37,22 +43,58 @@
 
 <section id="pokemonList">
 	{#if pokemonList && pokemonDetails.length > 0}
-		<ul>
+		<div id="pokemonContainerList">
 			{#each pokemonList.results as pokemon, index}
-			<li>
-				<img src={pokemonDetails[index]} alt={pokemon.name} />
-				{pokemon.name}
-			</li>
+				<a class="linkPokemon" href="/{pokemon.name}">
+					<div class="pokemonContainer" >
+						<img src={pokemonDetails[index]} alt={pokemon.name} />
+						<h3>{pokemon.name}<h3>
+					</div>
+				</a>
 			{/each}
-		</ul>
+		</div>
 	{:else}
 	<p>Chargement...</p>
 	{/if}
 </section>
 
 <style>
-	*{
-		margin: 0;
-		padding: 0;
+	@import "style.css";
+
+	h1{
+		color: #333333;
 	}
+
+	#pokemonContainerList {
+		display: flex;
+		justify-content: space-between;
+		height: calc(100vh - 100px);
+		overflow: auto;
+		flex-wrap: wrap;
+	}
+
+	.pokemonContainer{
+		display: flex;
+		width: 100px;
+		height: 125px;
+		flex-direction: column;
+		align-items: center;
+		margin: 10px;
+		padding: 20px;
+		border-radius: 10px;
+		background-color: #d3d1d1;
+	}
+	#titre {
+		display: flex;
+		justify-content: center;
+		height: 30px;
+		margin: 30px 0;
+		color: white;
+	}
+
+	.linkPokemon {
+		text-decoration: none;
+		color: #333333;
+	}
+	
 </style>
