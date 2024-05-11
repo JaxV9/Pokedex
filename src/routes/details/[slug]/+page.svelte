7321b7c4b9e59dbb7289f5e9a2ad63b21519cae5
JaxV9 <layanj.pro@gmail.com>
2024-05-11 10:43:37 +0200

    feat: some stats

--- src/routes/details/[slug]/+page.svelte
+++ src/routes/details/[slug]/+page.svelte
@@ -21,12 +21,12 @@
     fetchData();
 
     const test = () => {
-        console.log(pokemonData.types[0].type.name)
+        console.log(pokemonData.types)
     }
 </script>
     <Layout>
         {#if pokemonData !== null}
-        <section class="section">
+        <section class="pokemonSection">
         <div class="pokemonDatasContainer">
             <div class="pokemonPictureContainer">
                 <h1>{pokemonData.name}</h1>
@@ -34,23 +34,24 @@
             </div>
             <div class="pokemonStatsContainer">
                 <div class="stats">
-                    <span class="numberStats">Number : {pokemonData.order}</span>
-                    <div class="statsContainer">
-                        <div class="label">Types : </div>
-                        <div class="value">{pokemonData.types[0].type.name}</div>
-                    </div>
-                    <div class="statsContainer">
-                        <div class="label">Attaques : </div>
-                        <div class="value">
-                            <ul>
-                                <li>{pokemonData.moves[0]?.move.name}</li>
-                                <li>{pokemonData.moves[1]?.move.name}</li>
-                                <li>{pokemonData.moves[2]?.move.name}</li>
-                                <li>{pokemonData.moves[3]?.move.name}</li>
-                            </ul>
-                        </div>
-                        
-                    </div>
+                    <p>Taille :</p>
+                    <p>{pokemonData.height/10} m</p>
+                </div>
+                <div class="stats">
+                    <p>Poids :</p>
+                    <p>{pokemonData.weight/10} kg</p>
+                </div>
+                <div class="stats">
+                    <p>Types :</p>
+                    {#each pokemonData.types as types}
+                        <p>{types.type.name}</p>
+                    {/each}
+                </div>
+                <div class="stats">
+                    <p>Abilities :</p>
+                    {#each pokemonData.abilities as abilities}
+                        <p>{abilities.ability.name}</p>
+                    {/each}
                 </div>
             </div>
         </div>
@@ -64,21 +65,32 @@
 
 <style>
 
+    .pokemonSection {
+        margin-top: 50px;
+        padding-bottom: 50px;
+        width: 70%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .pokemonDatasContainer {
+    }
+
     .pokemonPictureContainer {
-        background-color: #F2F2F2;
         border-radius: 16px;
         padding: 16px;
+
     }
 
     .pokemonPictureContainer > h1 {
-        color: black;
         text-align: center;
         margin-bottom: 0;
         text-transform: capitalize;
+        font-size: 28px;
     }
 
     .pokemonPicture {
-        width: 100%;
+        width: 500px;
     }
 
     .pokemonDatasContainer {
@@ -95,10 +107,17 @@
         height: auto;
         display: flex;
         font-size: 20px;
+        background-color: #30A7D7;
+        padding: 16px;
+        border-radius: 16px;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+		column-gap: 16px;
+		row-gap: 16px;
     }
 
-    .stats {
-        margin: auto;
+    .stats > p {
+        text-align: center;
     }
 
     .statsContainer {
