96f81e548025ffb0673632a372687caefd85016a
JaxV9 <layanj.pro@gmail.com>
2024-05-12 19:04:55 +0200

    search page added

--- src/layout/Layout.svelte
+++ src/layout/Layout.svelte
@@ -5,19 +5,62 @@ import Pokédex_logo from '$lib/images/Pokédex_logo.png';
   
   <div class="layout">
     <div class="logoLayoutContainer">
-        <a href="/">
-            <img class="logoLayout" src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="" />
-        </a>
+        <div>
+            <a href="/">
+                <img class="logoLayout" src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="" />
+            </a>
+        </div>
+        <div class="navLinkContainer">
+            <a class="navLink" href="/search">
+                <div>
+                        Search
+                </div>
+            </a>
+        </div>
     </div>
     <slot></slot>
   </div>
 
 <style>
 
+.logoLayoutContainer {
+    width: 96%;
+    margin-top: 28px;
+    margin-left: auto;
+    margin-right: auto;
+}
 
 .logoLayout {
     width: 200px;
     cursor: pointer;
+    transition: all ease 0.2s;
+}
+
+.navLinkContainer {
+    display: grid;
+    grid-template-columns: repeat(auto-fill);
+	column-gap: 16px;
+    width: 70%;
+    margin-top: 28px;
+}
+
+.navLinkContainer > a {
+    border: 1px #FFFFFF solid;
+    text-align: center;
+    width: fit-content;
+}
+
+.navLink {
+    width: 100%;
+    color: #FFFFFF;
+    display: block;
+    padding: 8px 16px;
+    transition: all ease 0.2s;
+}
+
+.navLink:hover {
+    background-color: #FFFFFF;
+    color: #333333;
     transition: all ease 0.2s;
 }
 
@@ -26,11 +69,6 @@ import Pokédex_logo from '$lib/images/Pokédex_logo.png';
     transition: all ease 0.2s;
 }
 
-.logoLayoutContainer {
-    width: 96%;
-    margin-top: 28px;
-    margin-left: auto;
-    margin-right: auto;
-}
+
 
 </style>
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+	import Button from '$lib/components/ui/buttons/button.svelte';
+	import '../style.css';
+	import SearchBar from '$lib/components/ui/searchBar/searchBar.svelte';
+	import Layout from '../../layout/Layout.svelte';
+
+</script>
+  
+
+<svelte:head>
+	<title>Pokédex</title>
+	<meta name="description" content="Svelte demo app" />
+
+</svelte:head>
+<Layout>
+	<section class="section">
+		<h1>Find your pokemon</h1>
+		<SearchBar />
+	</section>
+</Layout>
+
+<style>
+
+
+ </style>
